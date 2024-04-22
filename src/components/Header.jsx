c2e07ff00e74a44687029a575b71aa1ebb72c7d1
arnaudrambourg <arnaudrambourg@gmail.com>
2024-04-22 21:10:22 +0200

    Header.jsx validate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,16 @@
 import { NavLink } from "react-router-dom";
 import Logo from '../pictures/LOGO-header.png';
 
+function Header () {
+    return (
+        <header className="navbar">
+            <NavLink to="/" ><img src={Logo} alt="Logo Header" className="navbar__img"/></NavLink>
+            <nav className="navbar__links">
+                <NavLink to="/" className={({isActive}) => isActive ? "navbar__links--active" : "navbar__links--inactive"}>Accueil</NavLink>
+                <NavLink to="/About"  className={({isActive}) => isActive ? "navbar__links--active" : "navbar__links--inactive"}>A propos</NavLink>
+            </nav>
+        </header>
+    )
+};
+
+export default Header
