fa385020f60787bd5d9574b74fe39b3a9df3a87f
arnaudrambourg <arnaudrambourg@gmail.com>
2024-04-26 20:03:07 +0200

    start to Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,25 @@
 import { useEffect, useState } from "react";
 import CardsAppartments from '../components/Card/Cards'
-import Homebanner from "../components/Banner/HomeBanner";
+import HomeBanner from "../components/Banner/HomeBanner";
 
 export default function Home(){
 
-    const [appartments], set
+    const [appartments, setAppartments] = useState([]);
+    useEffect(()=> {
+        fetch('/data/appartments-list.json')
+        .then(res => res.json())
+        .then(data => setAppartments(data))
+        .catch(error => console.log(error))
+    },[]);
+
+    return (
+        <>
+            <HomeBanner />
+            <div className="appartments-cards">
+                {appartments.map(apartment =>
+                    <CardsAppartments title={apartment.title} image={apartment.cover} id={apartment.id} key={apartment.id}/>
+                )}
+            </div>
+        </>
+    );
 }
