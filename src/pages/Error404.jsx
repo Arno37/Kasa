466d9e6ae071219ba9a3814ade0d5f9c234bc7df
arnaudrambourg <arnaudrambourg@gmail.com>
2024-05-03 16:15:19 +0200

    test about error.404 page

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Error404() {
   return (
-    <div>
-      <h1>Apartments</h1>
-    </div>
-  );
+    <main>
+      <div className='Error404'>
+    <p className='number'>404</p>
+    <p className='text'>Oups! La page que vous demandez n'existe pas.</p>
+    <Link to='/' className='link-error'>Retourner sur la page d'accueil</Link>
+  </div>
+  </main>
+  )
 }
 
 export default Error404;
