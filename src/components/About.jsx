095fdd98cb4d9950250f1fcc4b66cd471caca3db
arnaudrambourg <arnaudrambourg@gmail.com>
2024-05-06 10:01:46 +0200

    about component ok

---
+++ src/components/About.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import bannerAbout from '../pictures/about-banner.png';
+import Banner from '../components/Banner/Banner';
+import Apartments from "../pages/Apartments";
+
