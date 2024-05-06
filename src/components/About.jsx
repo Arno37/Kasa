130cb69846385f589545cf4de41ded92af2bd3eb
arnaudrambourg <arnaudrambourg@gmail.com>
2024-05-06 10:58:25 +0200

    starting to create about full component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,4 +2,19 @@ import React from "react";
 import bannerAbout from '../pictures/about-banner.png';
 import Banner from '../components/Banner/Banner';
 import Apartments from "../pages/Apartments";
+import Collapse from '../components/Collapse';
 
+function About() {
+    return (
+<main>
+    <Banner image={bannerAbout}/>
+
+    {Data.map((collapse , criteria) => (
+        <Collapse key={criteria} title= {collapse.title} description={collapse.description}/>
+    ))}
+</main>
+
+    )
+}
+
+export default About
